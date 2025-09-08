ae462f42a7b361ed10db712109c7e5c5c2bb1a9d
udaykiran-29 <udayguttula19221@gmail.com>
2025-09-08 22:19:07 +0530

    fix: correct API URL in frontend

--- Predusk/frontend/src/api.js
+++ Predusk/frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://dashboard.render.com/web/srv-d2vg083e5dus73flpfs0/deploys/dep-d2vg3t3uibrs738isea0' // Our backend API URL
+  baseURL: 'https://predusk-assessment-s0na.onrender.com' // Our backend API URL
 });
 
 export default api;
